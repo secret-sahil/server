66a79c3c330466236b792d5dfec4cb940cb09be0
Sahil Kumar <sahilkumar142002@gmail.com>
2024-04-25 14:32:04 +0530

    fixed bug

--- passport.js
+++ passport.js
@@ -6,7 +6,7 @@ passport.use(
 		{
 			clientID: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: "/auth/google/callback",
+			callbackURL: `${process.env.SERVER_URL}/auth/google/callback`,
 			scope: ["profile", "email"],
 		},
 		function (accessToken, refreshToken, profile, callback) {
